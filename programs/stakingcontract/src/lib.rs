e9420ddf91f2a93eab48266f513a9b004f915e1c
alxn787 <alenabraham787@gmail.com>
2025-06-20 10:45:11 +0530

    init

--- programs/stakingcontract/src/lib.rs
+++ programs/stakingcontract/src/lib.rs
@@ -13,9 +13,45 @@ pub mod stakingcontract {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePdaAccount<'info> {
+    #[account(mut)]
+    pub payer : Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + 32 + 4 + 8 + 1,
+        seeds = [b"user-stats",payer.key().as_ref()],
+        bump
+    )]
+
+    pub pda_acc : Account<'info, StakeAccount>,
+
+    pub system_program: Program<'info,System>
+}
+
+#[account]
+pub struct StakeAccount{
+    owner:Pubkey,
+    stakeamount: u32,
+    points:u32,
+    lasttimestamp:u64,
+    bump:u8
+}
 
 #[error_code]
 pub enum ErrorMessages {
-    msg!("amount must be greater than 0")
+    #[msg("amount must be greater than 0")]
+    InvalidAmount,
+    #[msg("Insufficient staked amount")]
+    InsufficientStake,
+    #[msg("Unautharized access")]
+    Unautharized,
+    #[msg("Arithmetic Overflow")]
+    Overflow,
+    #[msg("Arithmetic Underflow")]
+    Underflow,
+    #[msg("Invalid Timestamp")]
+    InvalidTimestamp
 }
+
